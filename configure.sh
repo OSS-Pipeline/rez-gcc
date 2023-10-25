45f26855a33d3fc9327b21655eb31ad4f9f5b0aa
Joshua Senouf <joshua.senouf@gmail.com>
2023-10-25 18:26:22 -0400

    Updated to GCC 11.2.0

--- configure.sh
+++ configure.sh
@@ -7,11 +7,11 @@ EXTRACT_PATH=$1
 BUILD_PATH=$2
 INSTALL_PATH=${REZ_BUILD_INSTALL_PATH}
 GCC_VERSION=${REZ_BUILD_PROJECT_VERSION}
-GMP_VERSION=$3
-MPFR_VERSION=$4
-MPC_VERSION=$5
-ISL_VERSION=$6
-CLOOG_VERSION=$7
+CLOOG_VERSION=$3
+GMP_VERSION=$4
+ISL_VERSION=$5
+MPC_VERSION=$6
+MPFR_VERSION=$7
 
 echo -e "\n"
 echo -e "================="
@@ -24,16 +24,16 @@ echo -e "[CONFIGURE][ARGS] EXTRACT PATH: ${EXTRACT_PATH}"
 echo -e "[CONFIGURE][ARGS] BUILD PATH: ${BUILD_PATH}"
 echo -e "[CONFIGURE][ARGS] INSTALL PATH: ${INSTALL_PATH}"
 echo -e "[CONFIGURE][ARGS] GCC VERSION: ${GCC_VERSION}"
-echo -e "[CONFIGURE][ARGS] GMP VERSION: ${GMP_VERSION}"
-echo -e "[CONFIGURE][ARGS] MPFR VERSION: ${MPFR_VERSION}"
-echo -e "[CONFIGURE][ARGS] MPC VERSION: ${MPC_VERSION}"
-echo -e "[CONFIGURE][ARGS] ISL VERSION: ${ISL_VERSION}"
 echo -e "[CONFIGURE][ARGS] CLOOG VERSION: ${CLOOG_VERSION}"
+echo -e "[CONFIGURE][ARGS] GMP VERSION: ${GMP_VERSION}"
+echo -e "[CONFIGURE][ARGS] ISL VERSION: ${ISL_VERSION}"
+echo -e "[CONFIGURE][ARGS] MPC VERSION: ${MPC_VERSION}"
+echo -e "[CONFIGURE][ARGS] MPFR VERSION: ${MPFR_VERSION}"
 echo -e "[CONFIGURE][ARGS] REZ REPO PATH: ${REZ_REPO_PAYLOAD_DIR}"
 
 # We check if the arguments variables we need are correctly set.
 # If not, we abort the process.
-if [[ -z ${EXTRACT_PATH} || -z ${BUILD_PATH} || -z ${INSTALL_PATH} || -z ${GCC_VERSION} || -z ${GMP_VERSION} || -z ${MPFR_VERSION} || -z ${MPC_VERSION} || -z ${ISL_VERSION} || -z ${CLOOG_VERSION} || -z ${REZ_REPO_PAYLOAD_DIR} ]]; then
+if [[ -z ${EXTRACT_PATH} || -z ${BUILD_PATH} || -z ${INSTALL_PATH} || -z ${GCC_VERSION} || -z ${CLOOG_VERSION} || -z ${GMP_VERSION} || -z ${ISL_VERSION} || -z ${MPC_VERSION} || -z ${MPFR_VERSION} || -z ${REZ_REPO_PAYLOAD_DIR} ]]; then
     echo -e "\n"
     echo -e "[CONFIGURE][ARGS] One or more of the argument variables are empty. Aborting..."
     echo -e "\n"
@@ -57,6 +57,13 @@ echo -e "[CONFIGURE][ARCHIVES] ISL-${ISL_VERSION}: ${ISL_ARCHIVE_PATH}"
 echo -e "[CONFIGURE][ARCHIVES] CLOOG-${CLOOG_VERSION}: ${CLOOG_ARCHIVE_PATH}"
 echo -e "\n"
 
+# CLooG
+if [ ! -L cloog ]; then
+    echo -e "[CONFIGURE][EXTRACT] Extracting CLooG-${CLOOG_VERSION} from ${CLOOG_ARCHIVE_PATH}..."
+
+    tar -xzf ${CLOOG_ARCHIVE_PATH} --directory ${EXTRACT_PATH}
+    ln -s ${EXTRACT_PATH}/cloog-${CLOOG_VERSION} ${EXTRACT_PATH}/cloog
+fi
 # GMP
 if [ ! -L gmp ]; then
     echo -e "[CONFIGURE][EXTRACT] Extracting GMP-${GMP_VERSION} from ${GMP_ARCHIVE_PATH}..."
@@ -65,12 +72,12 @@ if [ ! -L gmp ]; then
     ln -s ${EXTRACT_PATH}/gmp-${GMP_VERSION} ${EXTRACT_PATH}/gmp
     sed -e 's/M4=m4-not-needed/: # &/' -i ${EXTRACT_PATH}/gmp/configure
 fi
-# MPFR
-if [ ! -L mpfr ]; then
-    echo -e "[CONFIGURE][EXTRACT] Extracting MPFR-${MPFR_VERSION} from ${MPFR_ARCHIVE_PATH}..."
+# ISL
+if [ ! -L isl ]; then
+    echo -e "[CONFIGURE][EXTRACT] Extracting ISL-${ISL_VERSION} from ${ISL_ARCHIVE_PATH}..."
 
-    tar -xjf ${MPFR_ARCHIVE_PATH} --directory ${EXTRACT_PATH}
-    ln -s ${EXTRACT_PATH}/mpfr-${MPFR_VERSION} ${EXTRACT_PATH}/mpfr
+    tar -xjf ${ISL_ARCHIVE_PATH} --directory ${EXTRACT_PATH}
+    ln -s ${EXTRACT_PATH}/isl-${ISL_VERSION} ${EXTRACT_PATH}/isl
 fi
 # MPC
 if [ ! -L mpc ]; then
@@ -79,19 +86,12 @@ if [ ! -L mpc ]; then
     tar -xzf ${MPC_ARCHIVE_PATH} --directory ${EXTRACT_PATH}
     ln -s ${EXTRACT_PATH}/mpc-${MPC_VERSION} ${EXTRACT_PATH}/mpc
 fi
-# ISL
-if [ ! -L isl ]; then
-    echo -e "[CONFIGURE][EXTRACT] Extracting ISL-${ISL_VERSION} from ${ISL_ARCHIVE_PATH}..."
+# MPFR
+if [ ! -L mpfr ]; then
+    echo -e "[CONFIGURE][EXTRACT] Extracting MPFR-${MPFR_VERSION} from ${MPFR_ARCHIVE_PATH}..."
 
-    tar -xjf ${ISL_ARCHIVE_PATH} --directory ${EXTRACT_PATH}
-    ln -s ${EXTRACT_PATH}/isl-${ISL_VERSION} ${EXTRACT_PATH}/isl
-fi
-# CLooG
-if [ ! -L cloog ]; then
-    echo -e "[CONFIGURE][EXTRACT] Extracting CLooG-${CLOOG_VERSION} from ${CLOOG_ARCHIVE_PATH}..."
-
-    tar -xzf ${CLOOG_ARCHIVE_PATH} --directory ${EXTRACT_PATH}
-    ln -s ${EXTRACT_PATH}/cloog-${CLOOG_VERSION} ${EXTRACT_PATH}/cloog
+    tar -xjf ${MPFR_ARCHIVE_PATH} --directory ${EXTRACT_PATH}
+    ln -s ${EXTRACT_PATH}/mpfr-${MPFR_VERSION} ${EXTRACT_PATH}/mpfr
 fi
 
 # We run the configuration script of GCC.
--- package.py
+++ package.py
@@ -1,25 +1,17 @@
-
 # Based and improved from https://github.com/piratecrew/rez-gcc
 
 name = "gcc"
 
-version = "6.3.1"
+version = "11.2.0"
 
-authors = [
-    "GNU"
-]
-
-description = \
-    """
+description = """
     The GNU Compiler Collection (GCC) is a compiler system produced by the GNU Project
     supporting various programming languages.
     GCC is a key component of the GNU toolchain and the standard compiler for most projects
     related to GNU and Linux, including the Linux kernel.
     """
 
-variants = [
-    ["platform-linux"]
-]
+authors = ["GNU"]
 
 tools = [
     "gcc",
@@ -30,23 +22,37 @@ tools = [
     "gcc-ranlib",
     "gfortran",
     "gcc-nm",
-    "gcov"
+    "gcov",
+]
+
+requires = [
+    "cmake-3",
+]
+
+variants = [
+    [
+        "platform-linux",
+    ],
 ]
 
 build_system = "cmake"
 
+
 with scope("config") as config:
     config.build_thread_count = "logical_cores"
 
-uuid = "gcc-{version}".format(version=str(version))
 
 def commands():
     env.PATH.prepend("{root}/bin")
     env.CC.prepend("{root}/bin/gcc")
     env.CXX.prepend("{root}/bin/g++")
-    env.LD_LIBRARY_PATH.prepend("{root}/lib64:{root}/lib/gcc/x86_64-pc-linux-gnu/" + str(version))
+    env.LD_LIBRARY_PATH.prepend(
+        "{root}/lib64:{root}/lib/gcc/x86_64-pc-linux-gnu/{version}"
+    )
 
     # Helper environment variables.
     env.GCC_BINARY_PATH.set("{root}/bin")
     env.GCC_INCLUDE_PATH.set("{root}/include")
-    env.GCC_LIBRARY_PATH.set("{root}/lib64:{root}/lib/gcc/x86_64-pc-linux-gnu/" + str(version))
+    env.GCC_LIBRARY_PATH.set(
+        "{root}/lib64:{root}/lib/gcc/x86_64-pc-linux-gnu/{version}"
+    )
